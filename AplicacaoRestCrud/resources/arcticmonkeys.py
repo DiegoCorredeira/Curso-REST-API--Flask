03c6428722d70558d92f9dd500be1104a2c0ca81
Diego <diegosccpinto@gmail.com>
2023-09-25 12:33:10 -0300

    Removendo lista de musicas

--- AplicacaoRestCrud/resources/arcticmonkeys.py
+++ AplicacaoRestCrud/resources/arcticmonkeys.py
@@ -2,47 +2,6 @@ from flask import json
 from flask_restful import Resource, reqparse
 from models.arcticmonkeys import HumbugModel
 
-humbug_songs = [
-    {
-        'id': 1,
-        'title': 'My Propeller',
-        'artist': 'Arctic Monkeys',
-        'album': 'Humbug',
-        'release_date': '2009-08-19',
-        'time': '3:27',
-
-    },
-    {
-        'id': 2,
-        'title': 'Crying Lightning',
-        'artist': 'Arctic Monkeys',
-        'album': 'Humbug',
-        'release_date': '2009-08-19',
-        'time': '3:45',
-
-    },
-    {
-        'id': 3,
-        'title': 'Dangerous Animals',
-        'artist': 'Arctic Monkeys',
-        'album': 'Humbug',
-        'release_date': '2009-08-19',
-        'time': '3:30',
-
-    },
-    {
-        "id": 4,
-        "title": "Secret Door",
-        "artist": "Arctic Monkeys",
-        "album": "Humbug",
-        "release_date": "2009-08-19",
-        "time": "3:43",
-
-    }
-]
-
-
-
 class HumbugSongs(Resource):
     def get(self):
         return {'Humbug Songs': [song.json() for song in HumbugModel.query.all()]} # SELECT * FROM songs
