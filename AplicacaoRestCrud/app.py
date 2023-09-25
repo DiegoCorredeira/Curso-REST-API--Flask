21fabdbacb22ae07118d98890263261f0f4325ac
Diego <diegosccpinto@gmail.com>
2023-09-25 19:39:43 -0300

    Descompactando tuplas

--- AplicacaoRestCrud/app.py
+++ AplicacaoRestCrud/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-from resources.FWN import FWNSongs, FWNid
-from resources.WPSIATWIN import WPSAMTWINSongs, WPSAMTWINid
+from resources.FWN import FWNSongs, FWNId
+from resources.WPSIATWIN import WPSAMTWINSongs, WPSAMTWINId
 from resources.humbug import HumbugSongs, HumbugId
 from resources.user import User, UserRegister
 
@@ -16,17 +16,18 @@ def create_db():
     db.create_all()
 
 
-api.add_resource(HumbugSongs, '/humbug')
-api.add_resource(HumbugId, '/humbug/<int:humbug_id>')
-
-api.add_resource(WPSAMTWINSongs, '/WPSIATWIN')
-api.add_resource(WPSAMTWINid, '/WPSIATWIN/<int:WPSIATWIN_id>')
-
-api.add_resource(FWNSongs, '/FWN')
-api.add_resource(FWNid, '/FWN/<int:FWN_id>')
-
-api.add_resource(User, '/user/<int:user_id>')
-api.add_resource(UserRegister, '/register')
+resources = [
+    (HumbugSongs, '/humbug'),
+    (HumbugId, '/humbug/<int:humbug_id>'),
+    (WPSAMTWINSongs, '/WPSIATWIN'),
+    (WPSAMTWINId, '/WPSIATWIN/<int:WPSIATWIN_id>'),
+    (FWNSongs, '/FWN'),
+    (FWNId, '/FWN/<int:FWN_id>'),
+    (User, '/user/<int:user_id>'),
+    (UserRegister, '/register')
+]
+for resource, *routes in resources:
+    api.add_resource(resource, *routes)
 
 if __name__ == '__main__':
     from sql_alchemy import db
